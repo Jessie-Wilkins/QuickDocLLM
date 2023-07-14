92828ffc2b3d5b5ca1bcfd450e436a538e43d9cf
Jessie Wilkins <jessiekwilkins.yahoo.com@gmail.com>
2023-07-13 23:48:34 -0500

    Adding failed rust code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use llm_chain::executor;
+use llm_chain::options;
+use llm_chain::options::{ModelRef, Options};
+use std::error::Error;
+
+use llm_chain::{prompt::Data, traits::Executor};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
+
+    let model_type = "llama";
+    let model_path = "model/orca-mini-3b.ggmlv3.q4_1.bin";
+    let prompt = "Who was the president in 1985?";
+
+    let exec = executor!(
+        llama,
+        options!(
+            Model: ModelRef::from_path(model_path),
+            ModelType: model_type.to_string()
+        )
+    )?;
+    let res = prompt!(prompt).run(&parameters!(), &exec).await?;
+
+    println!("{}", res);
+    Ok(())
 }
